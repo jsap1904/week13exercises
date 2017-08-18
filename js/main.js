29802cb873689d35279973b8ce6da65c47a1016a
jsap1904 <jsapdevs@gmail.com>
2017-08-17 17:46:22 -0700

    update

--- js/main.js
+++ js/main.js
@@ -3,3 +3,23 @@ var string = "HELLOW WORLD"
 var stahpIt = string.toLowerCase();
 
 //Why is the toLowerCase() method useful?
+// great for search/ login inputs
+
+//Why is trim() useful?
+//removes white space or unecessary space
+
+//when can you use the trim() method?
+//file size minimization?
+
+//What is the difference between the split() and join() methods?
+// split() splits string into an array, join() joins array into string
+
+//What is one use of regular expressions in JavaScript?
+//modifying a search
+
+//What syntax is used to start and end a regular expression?
+//"\ blah \"
+
+//How could you use the test() method with a regular expression?
+//while searching with a regular expression, you would be able to apply 
+//or test the result to return true or false
